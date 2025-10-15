4867fb0bbbc067601b06c605e270a9a642eab591
Colum Cross <cxc9401@rit.edu>
2025-10-15 18:14:04 +0200

    Added the logo. Added fields to label the origin and destination of the trip the fare is for. Added the ability to remove a fare from the list of fares. Can preserve inputs to enter multiple of the same fare quickly.

--- main.js
+++ main.js
@@ -6,17 +6,39 @@
  * Start with just a simple function that calculates when the add button is clicked.
  */
 var totalFareCost = 0;
-function calculateFare() {
-  const fare = parseFloat(document.getElementById("newFare").value);
-  totalFareCost += fare;
+function addFare() {
+  const fare = isNaN(parseFloat(document.getElementById("newFare").value)) ? 0 : parseFloat(document.getElementById("newFare").value);
+
   
   const elem = document.createElement("li");
-  const newItem = document.createTextNode(fare + "€");
+  const itemText = document.getElementById("origin").value + " -> " + document.getElementById("destination").value + ": " + fare + "€ ";
+  const newItem = document.createTextNode(itemText);
   elem.appendChild(newItem);
+
+  // TODO: Add a button to remove the fare.
+  const removeButton = document.createElement("button");
+  removeButton.setAttribute("onClick", "removeFare(this.parentNode, "+fare+")");
+  removeButton.innerHTML = "Remove Fare";
+  elem.appendChild(removeButton);
+
   document.getElementById("fareList").appendChild(elem);
   
-  const newTotal = totalFareCost - parseFloat(document.getElementById("dticket").value);
+  calculateFare(fare);
 
+  if(!document.getElementById("clearValues").checked) {
+    document.getElementById("origin").value = '';
+    document.getElementById("destination").value = '';
+    document.getElementById("newFare").value = '';
+  }
+
+}
+function removeFare(listElement, fare) {
+  listElement.parentNode.removeChild(listElement);
+  const fareToRemove = 0 - parseFloat(fare);
+  calculateFare(fareToRemove);
+}
+function calculateFare(fare) {
+  totalFareCost += fare;
+  const newTotal = totalFareCost - parseFloat(document.getElementById("dticket").value);
   document.getElementById("totalSavings").innerHTML = newTotal + "€";
-  document.getElementById("newFare").value = '';
 }
